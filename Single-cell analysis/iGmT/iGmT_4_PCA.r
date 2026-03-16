fde536dcdfc203a6d3f7328a0e5c5501b5bcd821
LemaireLabAtSLU <143550230+LemaireLabAtSLU@users.noreply.github.com>
2026-03-16 14:32:31 -0500

    Update iGmT_4_PCA.r

--- Single-cell analysis/iGmT/iGmT_4_PCA.r
+++ Single-cell analysis/iGmT/iGmT_4_PCA.r
@@ -23,76 +23,16 @@ library(viridisLite)
 library("viridis")
 library(circlize)
 
-
-################################################################### Beginning of SLURM
-wd1 <- "/scratch/gpfs/LEVINE/llemaire/singleCellAnalysis/26.01.26_iGmT"
-setwd(wd1)
-object <- "26.01.26_iGmT2.RData"
-load(object)
-print("Environment Loaded")
-
-object <- "26.01.27_iGmT3.RData" ###Should be after load object, wrote over by load over
-inf <- sessionInfo()
-write(capture.output(inf),file = "26.01.27_sessionInfo_script3.txt")
-
-# split the dataset into a list of layer within the same object
-
-iGmT[["RNA"]] <- split(iGmT[["RNA"]], f = iGmT$orig.ident)
-
-# run standard anlaysis workflow
-iGmT <- FindVariableFeatures(iGmT, selection.method = "vst", nfeatures = 4000)
-iGmT <- ScaleData(iGmT, vars.to.regress = c("S.Score", "G2M.Score"), features = VariableFeatures(iGmT))
-
-print("Data are scaled")
-
-
-iGmT <- RunPCA(iGmT)
-DimPlot(iGmT, reduction = "pca")
-ggsave("PCA.pdf", device= "pdf", width = 20, 
-       height = 20, units = "cm")
-
-save.image(object)
-
-### Cluster and project without integration
-
-iGmT <- FindNeighbors(iGmT, dims = 1:30, reduction = "pca")
-iGmT <- FindClusters(iGmT, resolution = 0.6, cluster.name = "unintegrated_clusters")
-iGmT <- RunUMAP(iGmT, dims = 1:30, reduction = "pca", reduction.name = "umap.unintegrated")
-DimPlot(iGmT, reduction = "umap.unintegrated", group.by = c("orig.ident", "seurat_clusters"))
-ggsave("Unintegrated.UMAP.pdf", device= "pdf", width = 24, 
-       height = 16, units = "cm")
-
-print("Unintegrated data are projected")
-
-#Perform integration
-
-iGmT <- IntegrateLayers(object = iGmT, method = CCAIntegration, orig.reduction = "pca",
-                        new.reduction = "integrated.cca",
-                        verbose = TRUE)
-
-
-iGmT
-print("Integration Done")
-# re-join layers after integration
-
-iGmT[["RNA"]] <- JoinLayers(iGmT[["RNA"]])
-
-iGmT
-print("Layers joined")
-save.image(object)
-
-################################################################### End of SLURM
-
-wd1 <- "/scratch/gpfs/LEVINE/llemaire/singleCellAnalysis/26.01.26_iGmT"
+wd1 <- "/YourDirectory"
 setwd(wd1)
 
-object <- "26.01.27_iGmT3.RData"
+object <- "iGmT3.RData"
 
 load(object)
 
 iGmT
 
-object <- "26.01.27_iGmT4.RData"
+object <- "iGmT4.RData"
 
 #Choosing number of PCAs for clustering
 
